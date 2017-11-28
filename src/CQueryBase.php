c1b7fb501fd205eb52b582ab48128da8ce804ed5
sadkasadasd <2644218983@qq.com>
2017-11-28 18:45:08 +0800

    get one

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -266,7 +266,6 @@ class CQueryBase extends Model
 
     public static function getsWhereOrderPage( $arrWhere, $arrOrWhere,$arrOrderBy,$nPerPage=15, & $arrRtn, & $sDesc = "success" )
     {
-        $nErrCode = CErrCode::SUCCESS;
         if( is_array( $arrWhere ) && count( $arrWhere ) > 0 )
         {
 
