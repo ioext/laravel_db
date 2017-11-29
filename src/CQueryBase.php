c5893d1bc5596a645175c3413e8624f7bd5f419a
sadkasadasd <2644218983@qq.com>
2017-11-29 13:18:43 +0800

    get one

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -331,6 +331,7 @@ class CQueryBase extends Model
             if(  )//todo
             if(  )//todo
             if(  )//todo
+            if(  )//todo
         }
         elseif( $arrWhere == '' || count( $arrWhere ) == 0 || $arrWhere == null )
         {
