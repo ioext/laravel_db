570bae821ea3ba20a5038920d4c13dc9f0c11eb8
sadkasadasd <2644218983@qq.com>
2017-12-06 17:55:27 +0800

    init

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -251,7 +251,7 @@ class CQueryBase extends Model
             }
 
             $arrRtn = $arrRtn->toArray();
-            if( is_array( $arrRtn ) )
+            if( is_array( $arrRtn ) &&  )
 
             if( $arrRtn == null )
             {
