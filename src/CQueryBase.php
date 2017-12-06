08c2b92d15509e449533b81d0f8c08c83875c19e
sadkasadasd <2644218983@qq.com>
2017-12-06 17:53:40 +0800

    init

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -228,10 +228,7 @@ class CQueryBase extends Model
                     $arrRtn = self::query()->where( $arrWhere )->orWhere( $arrOrWhere );
                     foreach( $arrOrderBy as $nFKey => $arrFValue )
                     {
-                        foreach ( $arrFValue as $v )
-                        {
-                            $arrRtn->orderBy( $v[0], $v[1] );
-                        }
+                        $arrRtn->orderBy( $v[0], $v[1] );
                     }
 
                     if( is_int( $nPerPage ) && $nPerPage > 0 )
