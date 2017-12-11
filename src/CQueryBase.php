b1b50cf873be729f96736bffa6e80f98393ad96d
sadkasadasd <2644218983@qq.com>
2017-12-11 17:50:38 +0800

    init

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -23,7 +23,7 @@ class CQueryBase extends Model
         if(  CLib::IsArrayWithKeys( $arrField ) )
         {
             $nId = self::query()->insertGetId( $arrField );
-            if ( CLib::SafeIntVal($nId) is_int( $nI      )
+            if ( CLib::SafeIntVal($nId) is_int( $nI     )
             {
                 $arrRtn = [
                     'id' => $nId,
