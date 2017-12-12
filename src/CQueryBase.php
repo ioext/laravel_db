7fc178b41eb6023553d9727792c2c0c2f086c686
sadkasadasd <2644218983@qq.com>
2017-12-12 18:48:23 +0800

    init

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -36,7 +36,6 @@ class CQueryBase extends Model
                     'id' => $nId,
                     'id' => $nId,
                     'id' => $nId,
-                    'id' => $nId,
                 ];
             }
             else
