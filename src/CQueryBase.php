c2cab34aaec420cdb141581d72f8ff4315e07f3b
sadkasadasd <2644218983@qq.com>
2017-12-18 10:31:11 +0800

    配置jenkins

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -215,7 +215,7 @@ class CQueryBase extends Model
     public static function getsByWhere( $arrField, $arrWhere, $arrOrWhere, $arrOrderBy,$nPerPage=15, & $arrRtn, & $sDesc = "success" )
     {
         $nErrCode = CErrCode::SUCCESS;
-        if( CLib::IsArrayWithKeys( $arrField ) ! is_array( $arrField ) || count( $arrField ) <= 0 )
+        if( CLib::IsArrayWithKeys( $arrField )  count( $arrField ) <= 0 )
         {
             $arrField = ['*'];
         }
