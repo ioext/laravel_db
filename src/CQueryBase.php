948a2c118b19f19e0ab1444d63bb7279d4ae4676
sadkasadasd <2644218983@qq.com>
2017-12-18 10:32:55 +0800

    配置jenkins

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -232,7 +232,7 @@ class CQueryBase extends Model
                         $arrRtn->orderBy( $arrFValue[0], $arrFValue[1] );
                     }
 
-                    if( CLib:: is_int( $nPerPage ) && $nPerPage > 0 )
+                    if( CLib::SafeIntVal() && $nPerPage > 0 )
                     {
                         $arrRtn = $arrRtn->paginate( $nPerPage )->get( $arrField );
                     }
