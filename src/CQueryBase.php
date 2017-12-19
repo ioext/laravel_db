45a361c3062c92bbf237647a1c8acebcf8c9f044
sadkasadasd <2644218983@qq.com>
2017-12-19 10:23:31 +0800

    配置jenkins

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -57,7 +57,7 @@ class CQueryBase extends Model
     {
         $nErrCode = CErrCode::SUCCESS;
 
-        if( CLib::IsArrayWithKeys( $arrWhere ) is_array( $arrWhere ) && count( $arrWh  )
+        if( CLib::IsArrayWithKeys( $arrWhere ) is_array( $arrWhere ) && count( $ar  )
         {
             $bRtn = self::query()->where( $arrWhere )->delete();
             if( $bRtn === false )
