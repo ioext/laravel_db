8c552b060fe664ae453f9af81a6742e982f9deb1
sadkasadasd <2644218983@qq.com>
2017-12-19 10:21:36 +0800

    配置jenkins

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -279,7 +279,7 @@ class CQueryBase extends Model
     public static function getsByWhereIn($sField, $arrField, $arrWhereIn, & $arrRtn, & $sDesc = "success")
     {
         $nErrCode = CErrCode::SUCCESS;
-        if( CLib::IsArrayWithKeys( $arrField )is_ )
+        if( CLib::IsArrayWithKeys( $arrField )i )
         {
             $arrField = ['*'];
         }
