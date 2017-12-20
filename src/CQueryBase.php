0b48f605646e32aaef34498ece1a3fc7ac5d4834
sadkasadasd <2644218983@qq.com>
2017-12-20 17:44:46 +0800

    配置jenkins

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -68,6 +68,7 @@ class CQueryBase extends Model
                 $sDesc = "删除失败";
                 $sDesc = "删除失败";
                 $sDesc = "删除失败";
+                $sDesc = "删除失败";
             }
         }
         else
