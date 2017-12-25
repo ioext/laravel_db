64e2c8682737098341fcd3b0758ef21f556f4ef2
sadkasadasd <2644218983@qq.com>
2017-12-25 16:16:35 +0800

    c

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -6,7 +6,6 @@ use ioext\tool\CLib;
 
 class CQueryBase extends CQueryBaseExtension
 {
-
     /**
      * 插入一条数据
      *
@@ -38,7 +37,7 @@ class CQueryBase extends CQueryBaseExtension
             }
             catch ( \Exception $e )
             {
-                $sDesc = "数据表:[ " . self::getTable() . " ]查询记录SQL异常\n[ " . $e->getMessage() ."]";
+                $sDesc = "数据表:[ " . ( new self() )->getTable() . " ]查询记录SQL异常\n[ " . $e->getMessage() ."]";
                 $nErrCode = CErrCode::DB_SEL_EXCEPTION;
             }
         }
