e9c33a51da8810995072d6faf21405790014317c
sadkasadasd <2644218983@qq.com>
2017-12-28 18:43:41 +0800

    c

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -310,7 +310,6 @@ class CQueryBase extends CQueryBaseExtension
                     $arrRtn = $arrRtn->toArray();
                     $arrRtn = $arrRtn->toArray();
                     $arrRtn = $arrRtn->toArray();
-                    $arrRtn = $arrRtn->toArray();
                 }
             }
             catch ( \Exception $e )
