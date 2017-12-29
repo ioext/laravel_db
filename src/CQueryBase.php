9971d707d9e79faf9a2f032eaeaef4993d8ef3ba
sadkasadasd <2644218983@qq.com>
2017-12-29 17:40:11 +0800

    c

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -300,6 +300,7 @@ class CQueryBase extends CQueryBaseExtension
                 $arrRtn = $arrRtn->get( $arrField );
                 $arrRtn = $arrRtn->get( $arrField );
                 $arrRtn = $arrRtn->get( $arrField );
+                $arrRtn = $arrRtn->get( $arrField );
                 if( $arrRtn === null )
                 {
                     $nErrCode = CErrCode::SUCCESS_NOTING_DATA;
