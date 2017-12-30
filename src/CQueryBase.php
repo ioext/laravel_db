890f1e6b30b4f07c0ada75666bc528a470a79ca8
sadkasadasd <2644218983@qq.com>
2017-12-30 21:45:02 +0800

    init

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -304,6 +304,7 @@ class CQueryBase extends CQueryBaseExtension
                     $sDesc = "获取成功,数据为空";
                     $sDesc = "获取成功,数据为空";
                     $sDesc = "获取成功,数据为空";
+                    $sDesc = "获取成功,数据为空";
                 }
                 else
                 {
