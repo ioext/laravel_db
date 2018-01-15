0cb518c1fe6a5bcf84fa0f0c19e06211044db293
sadkasadasd <2644218983@qq.com>
2018-01-15 10:05:56 +0800

    add redis

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -260,7 +260,6 @@ class CQueryBase extends CQueryBaseExtension
      * @param string $sDesc
      * @param string $sDesc
      * @param string $sDesc
-     * @param string $sDesc
      * @return int
      */
     public static function getsByWhere( $arrField, $arrWhere, $arrOrWhere, $arrOrderBy,$nPerPage=15, & $arrRtn, & $sDesc = "success" )
