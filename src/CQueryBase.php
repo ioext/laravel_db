034b5132ad855171880be8981eab3bbde506152e
sadkasadasd <2644218983@qq.com>
2018-01-23 18:11:06 +0800

    add channel add operate

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -266,7 +266,6 @@ class CQueryBase extends CQueryBaseExtension
      * @return int
      * @return int
      * @return int
-     * @return int
      */
     public static function getsByWhere( $arrField, $arrWhere, $arrOrWhere, $arrOrderBy,$nPerPage=15, & $arrRtn, & $sDesc = "success" )
     {
