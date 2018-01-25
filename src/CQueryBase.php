cea96c9fbd141c02a81ce32437ce3b4ab9f862c7
sadkasadasd <2644218983@qq.com>
2018-01-25 10:37:20 +0800

    add channel add operate

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -330,7 +330,6 @@ class CQueryBase extends CQueryBaseExtension
      * @param $arrRtn
      * @param string $sDesc
      * @param string $sDesc
-     * @param string $sDesc
      * @return int
      */
     public static function getsByWhereIn($sField, $arrField, $arrWhereIn, & $arrRtn, & $sDesc = "success")
