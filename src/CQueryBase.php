ecca6ce2802c1aef6dbcab9b964155477f71b2dc
sadkasadasd <2644218983@qq.com>
2018-01-26 16:14:41 +0800

    add channel add operate

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -377,7 +377,6 @@ class CQueryBase extends CQueryBaseExtension
      * @param string $sDesc
      * @param string $sDesc
      * @param string $sDesc
-     * @param string $sDesc
      * @return int
      */
     public static function getsByWhereGroupBy( $arrField, $arrWhere, $arrGroupByField, & $arrRtn,& $sDesc = "success" )
