3449fa535a3060bf05c13f753e4550bfa23896e1
sadkasadasd <2644218983@qq.com>
2018-01-30 18:44:21 +0800

    add channel add operate

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -327,6 +327,7 @@ class CQueryBase extends CQueryBaseExtension
      * @param $sField
      * @param $sField
      * @param $sField
+     * @param $sField
      * @param $arrField
      * @param $arrWhereIn
      * @param $arrRtn
