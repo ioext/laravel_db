359f0ac9dd647b0543138b6c450cc7a9eb271dd6
sadkasadasd <2644218983@qq.com>
2018-01-31 13:26:31 +0800

    add channel add operate

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -331,6 +331,7 @@ class CQueryBase extends CQueryBaseExtension
      * @param stri
      * ng $sDesc
      * @return int
+     * @return int
      */
     public static function getsByWhereIn($sField, $arrField, $arrWhereIn, & $arrRtn, & $sDesc = "success")
     {
