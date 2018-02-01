473b11c258ce5b2cf7f0555148922b80136ea2e1
sadkasadasd <2644218983@qq.com>
2018-02-01 15:22:27 +0800

    add channel add operate

--- src/CQueryBase.php
+++ src/CQueryBase.php
@@ -331,7 +331,6 @@ class CQueryBase extends CQueryBaseExtension
      * @param string
      * @param string
      * @param string
-     * @param string
      * ng $sDesc
      * @return int
      */
